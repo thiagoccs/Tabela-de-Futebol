1f416bdabb2f97134cc05433acaf63209bba6794
Thiago Souza <thiago29.06@hotmail.com>
2023-02-27 18:58:13 -0300

    feat: criando migration cria tabela teams

---
+++ app/backend/src/database/migrations/20230227213824-cria-tabela-teams.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('teams', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    teamName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+   })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+  }
+};
