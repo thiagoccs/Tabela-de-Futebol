c46661307bda7c32515a89a3df1b986a730d3194
Thiago Souza <thiago29.06@hotmail.com>
2023-03-06 16:25:27 -0300

    feat: cria MatchController

---
+++ app/backend/src/api/controllers/MatchController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import IServiceMatch from '../interfaces/IServiceMatch';
+
+export default class MatchController {
+  private _service: IServiceMatch;
+
+  constructor(service: IServiceMatch) {
+    this._service = service;
+  }
+
+  async findAll(req: Request, res: Response) {
+    const allMatches = await this._service.findAll();
+
+    return res.status(200).json(allMatches);
+  }
+}
