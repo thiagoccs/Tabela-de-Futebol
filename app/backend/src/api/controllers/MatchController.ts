0b7a27d63f0ee58d636ae5715299c42d1e59d058
Thiago Souza <thiago29.06@hotmail.com>
2023-03-06 17:54:55 -0300

    fix: implementa roda '/matches/:id/finish'

--- app/backend/src/api/controllers/MatchController.ts
+++ app/backend/src/api/controllers/MatchController.ts
@@ -19,4 +19,12 @@ export default class MatchController {
 
     return res.status(200).json(allMatches);
   }
+
+  async changeMatch(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await this._service.finishedMatch(Number(id));
+
+    return res.status(200).json({ message: 'Finished' });
+  }
 }
--- app/backend/src/api/routes/matchRoutes.ts
+++ app/backend/src/api/routes/matchRoutes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import MatchController from '../controllers/MatchController';
+import validateToken from '../middlewares/checkToken.middle';
 import MatchService from '../services/MatchService';
 
 const matchService = new MatchService();
 const matchController = new MatchController(matchService);
 const matchRoutes = Router();
 
-matchRoutes.get('/', (req, res) => matchController.findAll(req, res));
+matchRoutes
+  .get('/', (req, res) => matchController.findAll(req, res));
+
+matchRoutes
+  .patch('/:id/finish', validateToken, (req, res) => matchController.changeMatch(req, res));
 
 export default matchRoutes;
